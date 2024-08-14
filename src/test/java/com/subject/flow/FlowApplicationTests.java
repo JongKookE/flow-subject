a8cd9147dcedf7d23bdaf60961a52dbbeb15a9d6
박종국 <whdrnrdl789@naver.com>
2024-08-14 19:34:13 +0900

    first commit

---
+++ src/test/java/com/subject/flow/FlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subject.flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
