329532092e1dd7e0549cf93df6e5f3d517f09038
박종국 <whdrnrdl789@naver.com>
2024-08-16 23:46:44 +0900

    feat: file upload

---
+++ src/main/java/com/subject/flow/file/controller/UploadController.java
@@ -0,0 +1,24 @@
+package com.subject.flow.file.controller;
+
+import com.subject.flow.file.service.UploadService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/files")
+public class UploadController {
+    private final UploadService uploadService;
+
+    @PostMapping
+    @ResponseBody
+    public String upload(@RequestParam("file") MultipartFile file) {
+        String fileName = this.uploadService.uploadFile(file);
+        return this.uploadService.toJsonForm(fileName);
+    }
+}
